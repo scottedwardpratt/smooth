ab5ca1f3bc9a4d4d4c3ecef579ef270cef6aca56
Karina Martirosova <martiros@msu.edu>
2022-01-31 15:37:43 -0500

    real

---
+++ software/src/real.h
@@ -0,0 +1,34 @@
+//#ifndef __SMOOTH_H__
+#define __SMOOTH_H__
+#include <cstdlib>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <array>
+#include <fstream>
+#include "parametermap.h"
+#include "misc.h"
+#include "constants.h"
+#include "randy.h"
+#include <list>
+
+using namespace std;
+
+class CReal{
+public:
+	CReal();
+	virtual void CalcRealY(vector<double> &y, vector<double> &theta);
+
+};
+
+/*class CSmoothEmulator : public CReal{
+	void CalcRealY(vector<double> &y, vector<double> &theta);
+	double CalcRealYFromRealA(vector<double> &theta);
+	void CalcYTrainFromRealA();
+	void RandomizeRealA();
+};
+*/
+class CReal_Taylor : public CReal{
+	CReal_Taylor();
+	void CalcRealY(vector<double> &y, vector<double> &theta);
+};
