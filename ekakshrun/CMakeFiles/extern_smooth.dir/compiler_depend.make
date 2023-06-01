432163fbf6f898177dca05d22b4f464f13ec5cb0
katariae <katariae@msu.edu>
2023-06-01 09:59:23 -0400

    worked on the keepscore file

---
+++ ekakshrun/CMakeFiles/extern_smooth.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for extern_smooth.
+# This may be replaced when dependencies are built.
--- ekakshrun/try1.cc
+++ ekakshrun/try1.cc
@@ -12,28 +12,22 @@ int main(int argc,char *argv[]){
 		exit(1);
 	}
 	CparameterMap *parmap=new CparameterMap();
-	double y,yreal,accuracy,r2,average_accuracy=0.0,average_expected_accuracy=0.0,sigmay2,ybar,y2bar;
+	double y,yreal,accuracy,average_accuracy=0.0,average_expected_accuracy=0.0,sigmay2,ybar,y2bar,SigmaYreal;
 	unsigned int isample,itest,ntest=25,ipar,ireal,nreal=10;
 	vector<double> Theta;
 	// This plays the role of the "real" model
 	CReal_Taylor *real;
 
 	string parfilename="parameters/"+string(argv[1]);
-	printf("parfilename=%s\n",parfilename.c_str());
 	parmap->ReadParsFromFile(parfilename);
-	printf("check aa\n");
 	CSmoothEmulator emulator(parmap);
-		printf("check a: MaxRank=%d\n",emulator.smooth->MaxRank);
 
 	Theta.resize(emulator.NPars);
 	emulator.randy->reset(-time(NULL));
-	printf("check b: MaxRank=%d\n",emulator.smooth->MaxRank);
 	real=new CReal_Taylor(emulator.NPars,emulator.smooth->MaxRank,emulator.randy);
 	real->LAMBDA=emulator.LAMBDA;
 	emulator.real=real;
-
-	printf("check c : MaxRank=%d\n",emulator.smooth->MaxRank);
-
+	
 	emulator.SetThetaSimplex();
 	printf("Set %d Training Points\n",emulator.NTrainingPts);
 
@@ -47,42 +41,33 @@ int main(int argc,char *argv[]){
 		printf("------ ireal=%d -----\n",ireal);
 		accuracy=0.0;
 		real->RandomizeA(100.0);
-		real->A[0]=0.0;
-		//
+		//real->A[0]=0.0;
+		//  This is just for testing, to make sure that the emulator exactly reproduces training points
 		emulator.CalcYTrainFromThetaTrain();
 		emulator.GenerateASamples();
-		for(itest=0;itest<emulator.NTrainingPts;itest++)
-		{
+		for(itest=0;itest<emulator.NTrainingPts;itest++){
 			yreal=emulator.YTrain[itest];
 			y=emulator.smooth->CalcY(emulator.ASample[1],emulator.LAMBDA,emulator.ThetaTrain[itest]);
-			if(fabs(yreal-y)>0.001)
-			{
+			if(fabs(yreal-y)>0.001){
 				printf("Emulator fails!\n");
 				printf("%u, %8.4f: %g =? %g\n",itest,emulator.ThetaTrain[itest][0],y,yreal);
 			}
 		}
+		//
 
+		// This tests accuracy of emulator and compares to estimated accuracy,
+		// estimated accuracy should only be correct when averaged over many real functions
 		accuracy=sigmay2=0.0;
-		for(itest=0;itest<ntest;itest++)
-		{
-			for(ipar=0;ipar<emulator.NPars;ipar++)
-			{
-				if(emulator.NPars==1)
-				{
+		for(itest=0;itest<ntest;itest++){
+			for(ipar=0;ipar<emulator.NPars;ipar++){
+				if(emulator.NPars==1){
 					Theta[ipar]=-1.0+(2.0/double(ntest))*(0.5+itest);
 				}
-				else
-				{
-					do
-					{
-						r2=0.0;
-						Theta[ipar]=1.0-2.0*emulator.randy->ran();
-						r2+=Theta[ipar]*Theta[ipar];
-					}
-					while(r2>1.0);
+				else{
+					Theta[ipar]=1.0-2.0*emulator.randy->ran();
 				}
 			}
-			yreal=real->CalcY(Theta);
+			real->CalcY(Theta,yreal,SigmaYreal);
 			ybar=y2bar=0.0;
 			for(isample=0;isample<emulator.NASample;isample++){
 				y=emulator.smooth->CalcY(emulator.ASample[isample],emulator.LAMBDA,Theta);
@@ -91,24 +76,24 @@ int main(int argc,char *argv[]){
 			}
 			y2bar=y2bar/double(emulator.NASample);
 			ybar=ybar/double(emulator.NASample);
-			printf("ybar=%g =? %g\n",ybar,yreal);
-
+			printf("ybar=%8.4f =? %8.4f\n",ybar,yreal);
+			
 			accuracy+=(ybar-yreal)*(ybar-yreal);
 			sigmay2+=y2bar-ybar*ybar;
 		}
 		accuracy=accuracy/ntest;
-		sigmay2=sigmay2/ntest;
+		sigmay2=sigmay2/double(ntest-1);
 		accuracy=sqrt(accuracy);
-		sigmay2=sqrt(sigmay2/2.0);
+		sigmay2=sqrt(sigmay2);
 		printf("accuracy=%7.3f, expected accuracy=%7.3f\n",accuracy,sigmay2);
 		average_accuracy+=accuracy;
 		average_expected_accuracy+=sigmay2;
-
+		
 	}
 	average_accuracy=average_accuracy/double(nreal);
 	average_expected_accuracy=average_expected_accuracy/double(nreal);
-
-	printf("<accuracy>=%g, <expected accuracy>=%g\n",average_accuracy,average_expected_accuracy);
-
+	
+	printf("average <accuracy>=%g, average <expected accuracy>=%g\n",average_accuracy,average_expected_accuracy);
+	
 	return 0;
 }
