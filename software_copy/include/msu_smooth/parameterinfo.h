6515a244470f0d73afad26e16defe5cb0bc1d189
scottpratt <prattsc@msu.edu>
2023-06-07 14:33:55 -0400

    yada yada

--- software_copy/include/msu_smooth/parameterinfo.h
+++ software_copy/include/msu_smooth/parameterinfo.h
@@ -21,36 +21,27 @@
 
 class CPriorInfo{
 public:
-	vector<string> name;
-	string filename;
-	
-	vector<double> min, max;
-	double  width, center;
-	double theta;
-	int param_num;
-	
-	CPriorInfo(string file, vector<double> x_min, vector<double> x_max);
-	CPriorInfo(vector<double> x_min, vector<double> x_max);
-	double CParameterTranslateTheta_to_x(vector<double> min, vector<double> max,double theta, string interval);
-	double CParameterTranslateX_to_Theta(vector<double> x_min, vector<double> x_max, double x, string interval);
-	void Print(string &name, string &type, double &min, double &max);
-	void Write(vector<double> &xval, vector<double> &yval,string filename);
-//	ReadInfo();
+	CPriorInfo(string parinfo_filename);
+	int NModelPars;
+	string parinfo_filename;
+	vector<string> parname,type; // type is gaussian or linear
+	vector<double> xmin, xmax;
 };
 
 
 
-class CModelParameter{
+class CModelParameters{
 public:
 //has actual values of the parameter
-//each object tells you the point if you run 100 points will have 100 objects
-	CModelParameter(vector<double> x_val, vector<double> theta_val, CPriorInfo *info_val);
 	vector<double> x;
 	vector<double> theta;
-	CPriorInfo *info;
+	CModelParameters(CPriorInfo *priorinfo_set);
+	void TranslateTheta_to_x();
+	void TranslateX_to_Theta();
+	void Print();
 
-
-
-
+	int NModelPars;
+	CPriorInfo *priorinfo;
+	
 };
 #endif
