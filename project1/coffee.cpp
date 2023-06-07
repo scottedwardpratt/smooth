ee3b520d693c28409df5a813698ad9f7bc917105
QuirrenSR <erdogan1@msu.edu>
2023-06-07 11:26:15 -0400

    Adjusted coffee.cpp to have a log_file and read numbers from that. Additionally it creates the run directories now.

--- project1/coffee.cpp
+++ project1/coffee.cpp
@@ -2,50 +2,34 @@
 #include <cmath>
 #include <time.h>
 #include <cstdio>
-#include<vector>
-using namespace std;
-#include<cstdlib>
+#include <vector>
+#include <cstdlib>
 #include <fstream>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-class Run
-{
-public:
-  static
-  int count;
-  int no;
-
-  Run()
-  {
-    no = count;
-    count++ ;
-  }
-
-  void MakeDir()
-  {
-    int stat;
-    string path_s = "git/smooth/project1/modelruns/run%d", no;
-
-    stat = _mkdir(path_s);
-
-    if(stat == -1){
-      cout << "Failed " << endl;
-    }
-  }
-
-};
 
 using namespace std;
 
 int main()
 {
-  Run new_run;
-  new_run = new Run();
-  new_run.MakeDir();
+  int runNo;
+  FILE *fptr;
+
+
+  ifstream file;
+  file.open("log_file.txt");
+
+  file >> runNo;
+
+  cout << runNo << endl;
+
+  string path = "run" + std::to_string(runNo);
+  const char *dirname = path.c_str();
+
+  mkdir(dirname, 0777);
+
+  fptr = fopen("log_file.txt","w");
+
+  fprintf(fptr,"%d",runNo + 1);
 
   return 0;
 }
