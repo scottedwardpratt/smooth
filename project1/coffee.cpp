6d2f6dd91bd0798d3c403ff55b8dfbd9a498c4e7
QuirrenSR <erdogan1@msu.edu>
2023-06-07 12:14:50 -0400

    sadad

--- project1/coffee.cpp
+++ project1/coffee.cpp
@@ -13,22 +13,16 @@ int main()
 {
   int runNo;
   FILE *fptr;
-
-
   ifstream file;
+
   file.open("log_file.txt");
-
   file >> runNo;
-
-  cout << runNo << endl;
 
   string path = "run" + std::to_string(runNo);
   const char *dirname = path.c_str();
-
   mkdir(dirname, 0777);
 
   fptr = fopen("log_file.txt","w");
-
   fprintf(fptr,"%d",runNo + 1);
 
   return 0;
