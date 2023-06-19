783a57ec8a4e1ae7eff0eb9241f320a0102c9350
katariae <katariae@msu.edu>
2023-06-19 13:54:42 -0400

    ig

--- project1/obs_test2.cpp
+++ project1/obs_test2.cpp
@@ -62,3 +62,37 @@ int main()
     return 0;
 
   }
+
+  string s;
+  int i;
+  double min,max,exp_val;
+  vector<double> min_vec;
+  //vector<double> exp_val;
+  FILE *fptr;
+  fptr = fopen("obs.txt","w");
+
+  FILE *fp;
+  fp = fopen ("Info/mod_parameters_info.txt","r");
+  if (fp!=NULL)
+  {
+    for(int i = 1; i < 10; i ++)
+    {
+        fscanf(fp,"par%d %s %lf %lf\n",&i,&s,&min,&max);
+        min_vec.push_back(min);
+
+        exp_val = min_vec[3];
+
+
+
+
+
+    }
+    fprintf(fptr,"obs%d %s %10.3f %10.3f\n",i,s.c_str(),exp_val,0.001);
+    cout << min_vec[1] << endl;
+    fclose (fp);
+  }
+
+  //cout <<
+  fclose(fptr);
+  return 0;
+>>>>>>> cc96b5d (nsn)
