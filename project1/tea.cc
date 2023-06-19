29d23a1b06420cbfeaec9974015ec697900e4655
QuirrenSR <erdogan1@msu.edu>
2023-06-19 16:26:44 -0400

    ejnf

---
+++ project1/tea.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <time.h>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+#include <sys/stat.h>
+#include "msu_smooth/parameterinfo.h"
+#include "msu_commonutils/parametermap.h"
+#include "msu_commonutils/constants.h"
+#include "msu_smooth/smooth.h"
+#include "msu_smooth/emulator.h"
+#include "msu_commonutils/gslmatrix.h"
+#include "msu_commonutils/log.h"
+#include "msu_smooth/simplex.h"
+#include "msu_smooth/scorecard.h"
+
+
+int main() {
+  /* code */
+  return 0;
+}
