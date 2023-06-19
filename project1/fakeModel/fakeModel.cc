163027adc5fe4510557d9e0eab8913c2f2e7021b
QuirrenSR <erdogan1@msu.edu>
2023-06-19 13:33:20 -0400

    hfhg

--- project1/fakeModel/fakeModel.cc
+++ project1/fakeModel/fakeModel.cc
@@ -6,8 +6,4 @@ using namespace std;
 static vector<double> fakeModel(par1, par2, par3, par4, par5, par6, par7, par8, par9){
 
 
-  
-
-
-
 }
--- project1/obs_test1.cc
+++ project1/obs_test1.cc
@@ -1,48 +1,54 @@
 #include <iostream>
 #include <cmath>
-#include <time.h>
 #include <cstdio>
-#include<vector>
-using namespace std;
-#include<cstdlib>
+#include <vector>
+#include <cstdlib>
 #include <fstream>
 
-
-
-
+using namespace std;
 
 int main()
 {
-  string s;
-  int i;
-  double min,max,vals;
+  char s,s1;
+  double val,theta;
+  vector<double> par_vals;
   vector<double> obs;
-  //FILE *fptr;
-  //fptr = fopen("obs.txt","w");
+
+  for (size_t i = 0; i < 10; i++) {
+
+  FILE *fptr;
+  string obs_dir = "modelruns/run" + to_string(i) + "/obs.txt";
 
   FILE *fp;
-  fp = fopen ("Info/mod_parameters_info.txt","r");
-  if (fp!=NULL)
-  {
-    for(int i = 1; i < 10; i ++)
+  string dirname = "modelruns/run" + to_string(i) + "/mod_parameters.txt";
+  fp = fopen(dirname.c_str(),"r");
+
+    for(int i = 1; i < 10; i++)
     {
-      fscanf(fp,"par%d %s %lf %lf\n",&i,&s,&min,&max);
-      while(min == 0.336) //loop forever
-      {
-          obs.push_back(min); //otherwise add it to the vector
-      }
+      fscanf(fp,"   %s (%s): x=%lf, theta=%lf\n",&s1,&s,&val,&theta);
+      par_vals.push_back(val);
+    }
+
+    for (size_t i = 0; i < 9; i++)
+    {
+      obs.push_back(par_vals[i] + par_vals[i+1]);
+    }
+
+    for (size_t i = 0; i < 3; i++)
+    {
+      obs.push_back(par_vals[i] * par_vals[i + 3]);
+    }
 
 
-      //for(int i = 1; i<10; i= i+3 ){
-      cout << obs[1]<< endl;
-      //}
-      //fprintf(fptr,"obs%d %s %10.3f %10.3f\n",i,s.c_str(),10.99,0.001);
+    int obs_length = obs.size();
+
+    for (size_t i = 0; i < obs_length; i++) {
+      fptr = fopen(obs_dir.c_str(),"a");
+      fprintf(fptr,"obs%ld %f",i, obs[i]);
+      fclose (fptr);
     }
     fclose (fp);
   }
-
-  //cout <<
-  //fclose(fptr);
   return 0;
 
 
