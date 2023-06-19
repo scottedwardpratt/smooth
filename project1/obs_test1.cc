daf654fcbb5ac72dcdeee0dcbc5cb48efcfeea98
katariae <katariae@msu.edu>
2023-06-19 12:28:26 -0400

    obstest

--- project1/obs_test1.cc
+++ project1/obs_test1.cc
@@ -13,30 +13,37 @@ using namespace std;
 
 int main()
 {
-    string s;
-    int i;
-    double min,max;
-    FILE *fptr;
-    fptr = fopen("obs.txt","w");
+  string s;
+  int i;
+  double min,max,vals;
+  vector<double> obs;
+  //FILE *fptr;
+  //fptr = fopen("obs.txt","w");
 
-    FILE *fp;
-    fp = fopen ("Info/mod_parameters_info.txt","r");
-    if (fp!=NULL)
-    {
-      for(int i = 1; i < 10; i ++)
-      {
-          fscanf(fp,"par%d %s      %10.3f      %10.3f\n",&i,&s,&min,&max);
-      }
-      fclose (fp);
-    }
+  FILE *fp;
+  fp = fopen ("Info/mod_parameters_info.txt","r");
+  if (fp!=NULL)
+  {
     for(int i = 1; i < 10; i ++)
     {
-        fprintf(fptr,"par%d %s %10.3f %10.3f\n",i,s.c_str(),min,max);
-    }
+      fscanf(fp,"par%d %s %lf %lf\n",&i,&s,&min,&max);
+      while(min == 0.336) //loop forever
+      {
+          obs.push_back(min); //otherwise add it to the vector
+      }
 
-    //cout <<
-    fclose(fptr);
-    return 0;
+
+      //for(int i = 1; i<10; i= i+3 ){
+      cout << obs[1]<< endl;
+      //}
+      //fprintf(fptr,"obs%d %s %10.3f %10.3f\n",i,s.c_str(),10.99,0.001);
+    }
+    fclose (fp);
+  }
+
+  //cout <<
+  //fclose(fptr);
+  return 0;
 
 
 }
