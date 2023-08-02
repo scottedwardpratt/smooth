e1334d14e679d3ab75abebe94407ea309ea4d3e2
QuirrenSR <erdogan1@msu.edu>
2023-08-02 14:45:30 -0400

    dlfkgjd

---
+++ bin/CMakeFiles/smoothy_writecoefficients.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smoothy_writecoefficients.dir/codes/smoothy_writecoefficients.cc.o"
+  "CMakeFiles/smoothy_writecoefficients.dir/codes/smoothy_writecoefficients.cc.o.d"
+  "smoothy_writecoefficients"
+  "smoothy_writecoefficients.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smoothy_writecoefficients.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
