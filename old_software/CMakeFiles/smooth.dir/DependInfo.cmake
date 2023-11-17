6bc20862ad42a9d13ce3199f1eec3e3f360a0bfd
scottpratt <prattsc@msu.edu>
2023-11-17 13:56:31 -0500

    yada yada

---
+++ old_software/CMakeFiles/smooth.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/scottpratt/git/smooth/software/src/emulator.cc" "CMakeFiles/smooth.dir/src/emulator.cc.o" "gcc" "CMakeFiles/smooth.dir/src/emulator.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/latincube.cc" "CMakeFiles/smooth.dir/src/latincube.cc.o" "gcc" "CMakeFiles/smooth.dir/src/latincube.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/master.cc" "CMakeFiles/smooth.dir/src/master.cc.o" "gcc" "CMakeFiles/smooth.dir/src/master.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/modelparinfo.cc" "CMakeFiles/smooth.dir/src/modelparinfo.cc.o" "gcc" "CMakeFiles/smooth.dir/src/modelparinfo.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/observableinfo.cc" "CMakeFiles/smooth.dir/src/observableinfo.cc.o" "gcc" "CMakeFiles/smooth.dir/src/observableinfo.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/pca.cc" "CMakeFiles/smooth.dir/src/pca.cc.o" "gcc" "CMakeFiles/smooth.dir/src/pca.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/prior.cc" "CMakeFiles/smooth.dir/src/prior.cc.o" "gcc" "CMakeFiles/smooth.dir/src/prior.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/real.cc" "CMakeFiles/smooth.dir/src/real.cc.o" "gcc" "CMakeFiles/smooth.dir/src/real.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/scorecard.cc" "CMakeFiles/smooth.dir/src/scorecard.cc.o" "gcc" "CMakeFiles/smooth.dir/src/scorecard.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/simplex.cc" "CMakeFiles/smooth.dir/src/simplex.cc.o" "gcc" "CMakeFiles/smooth.dir/src/simplex.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/smooth.cc" "CMakeFiles/smooth.dir/src/smooth.cc.o" "gcc" "CMakeFiles/smooth.dir/src/smooth.cc.o.d"
+  "/Users/scottpratt/git/smooth/software/src/traininginfo.cc" "CMakeFiles/smooth.dir/src/traininginfo.cc.o" "gcc" "CMakeFiles/smooth.dir/src/traininginfo.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
