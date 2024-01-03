c54a1712bdd7048cc70fdfdb69131210bb3d82e0
scottpratt <prattsc@msu.edu>
2024-01-03 13:55:39 -0500

    yada yada

--- software/src/smooth.cc
+++ software/src/smooth.cc
@@ -200,59 +200,6 @@ void CSmooth::InitArrays(){
 	}
 }
 
-double CSmooth::CalcY(vector<double> &A,double LAMBDA,vector<double> &theta){
-	unsigned int ic,ir;
-	double answer=0.0,term,rfactor;
-	rfactor=GetRFactor(LAMBDA,theta);
-	answer=0.0;
-	for(ic=0;ic<NCoefficients;ic++){
-		term=A[ic]*sqrt(double(dupfactor[ic])/double(factorial[rank[ic]]));
-		for(ir=0;ir<rank[ic];ir++){
-			term*=theta[IPar[ic][ir]]/LAMBDA;
-//			cout << "theta is:" << theta[IPar[ic][ir]] << endl;
-		}
-		answer+=term;
-	}
-	answer*=rfactor;
-
-
-	return answer;
-}
-
-double CSmooth::CalcY_Remainder(vector<double> &A,double LAMBDA,vector<double> &theta,unsigned int NTrainingPts){
-	unsigned int ic,ir;
-	double answer=0.0,term,rfactor;
-	rfactor=GetRFactor(LAMBDA,theta);
-	answer=0.0;
-	for(ic=NTrainingPts;ic<NCoefficients;ic++){
-		term=A[ic]*sqrt(double(dupfactor[ic])/double(factorial[rank[ic]]));
-		for(ir=0;ir<rank[ic];ir++){
-			term*=theta[IPar[ic][ir]]/LAMBDA;
-		}
-		answer+=term;
-	}
-	answer*=rfactor;
-	return answer;
-}
-
-double CSmooth::CalcY_Remainder_FromMtot(vector<double> &A,unsigned int NTrainingPts,vector<double> &Mtot){
-	double answer=0.0;
-	unsigned int ic;
-	for(ic=NTrainingPts;ic<NCoefficients;ic++){
-		answer+=Mtot[ic]*A[ic];
-	}
-	return answer;
-}
-
-double CSmooth::CalcY_FromMtot(vector<double> &A,vector<double> &Mtot){
-	double answer=0.0;
-	unsigned int ic;
-	for(ic=0;ic<NCoefficients;ic++){
-		answer+=Mtot[ic]*A[ic];
-	}
-	return answer;
-}
-
 double CSmooth::GetRFactor(double LAMBDA,vector<double> &theta){
 	unsigned int ir,ipar,NPars=theta.size();
 	double r2=0.0,answer;
