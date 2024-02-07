83aeb971445488759c26896f2dde819f06cf55fd
scottpratt <prattsc@msu.edu>
2024-02-07 15:40:36 -0600

    yada yada

--- software/src/tune_exact.cc
+++ software/src/tune_exact.cc
@@ -4,57 +4,9 @@ using namespace std;
 using namespace NBandSmooth;
 using namespace NMSUPratt;
 
-CSmoothEmulator::TuneExact(){
-	CalcMForTraining();
-	observable_name=observable_name_set;
-	NTrainingPts=smoothmaster->traininginfo->NTrainingPts;
 
-}
-
-
-
-
-void CSmoothEmulator::SetThetaTrain(){
-	ThetaTrain.resize(NTrainingPts);
-	for(unsigned int itrain=0;itrain<NTrainingPts;itrain++){
-		ThetaTrain[itrain].resize(NPars);
-		for(unsigned int ipar=0;ipar<NPars;ipar++){
-			ThetaTrain[itrain][ipar]=smoothmaster->traininginfo->modelpars[itrain]->Theta[ipar];
-		}
-	}
-}
-
-void CSmoothEmulator::Init(){
-	SigmaA=SigmaA0;
-	NSigmaA=0;
-	SigmaAbar=0.0;
-	FirstTune=true;
-	MCStepSize=MCStepSize/double(NPars*NPars);
-	MCSigmaAStepSize=MCSigmaAStepSize/double(NPars*NPars);
-
-	ASample.resize(NASample);
-	for(unsigned int isample=0;isample<NASample;isample++){
-		ASample[isample].resize(smooth->NCoefficients);
-		//SetA_RanGauss(SigmaA,ASample[isample]);
-		SetA_Zero(ASample[isample]);
-	}
-
-	A.resize(smooth->NCoefficients);
-	SetA_Zero(A);
-	ATrial.resize(smooth->NCoefficients);
-	SetA_Zero(ATrial);
-	M.resize(NTrainingPts,NTrainingPts);
-	Minv.resize(NTrainingPts,NTrainingPts);
-	Mtot.resize(NTrainingPts);
-	for(unsigned int it=0;it<NTrainingPts;it++){
-		Mtot[it].resize(smooth->NCoefficients);
-		for(unsigned int ic=0;ic<smooth->NCoefficients;ic++){
-			Mtot[it][ic]=0.0;
-		}
-	}
-}
-
-void CSmoothEmulator::Tune(){
+void CSmoothEmulator::TuneExact(){
+	unsigned int NCoefficients=smooth->NCoefficients;
 	unsigned int itrain,ic,a,b,c;
 	CalcMForTraining();
 	Eigen::VectorXd alpha,gamma,YTrain,delta;
@@ -70,16 +22,16 @@ void CSmoothEmulator::Tune(){
 	C.setZero();
 	delta.setZero();
 	Abest.resize(NCoefficients);
-	double betadotbetab,betaadotbetac,betabdotbetac;
+	double betaadotbetab,betaadotbetac,betabdotbetac;
 	YTrain.resize(NTrainingPts);
-	for(itrain=0;itrain<NTrainingPtsitrain++){
+	for(itrain=0;itrain<NTrainingPts;itrain++){
 		YTrain[itrain]=smoothmaster->traininginfo->YTrain[iY][itrain];
 	}
 	alpha=Minv*YTrain;
-	for(a=0;a<NTrain;a++){
-		for(b=NTrain;b<NCoefficients;b++){
+	for(a=0;a<NTrainingPts;a++){
+		for(b=NTrainingPts;b<NCoefficients;b++){
 			for(itrain=0;itrain<NCoefficients;itrain++){
-				beta(a,b)+=Minv(a,itrain)*Mtot(itrain,b);
+				beta(a,b)+=Minv(a,itrain)*Mtot[itrain][b];
 			}
 		}
 	}
@@ -87,7 +39,7 @@ void CSmoothEmulator::Tune(){
 	for(b=0;b<NTrainingPts;b++){
 		for(c=0;c<NTrainingPts;c++){
 			betabdotbetac=0.0;
-			for(ic=NTrainingPts;ic<NCoefficienta;ic++){
+			for(ic=NTrainingPts;ic<NCoefficients;ic++){
 				betabdotbetac+=beta(b,ic)*beta(c,ic);
 			}
 			C(b,c)+=betabdotbetac;
@@ -108,20 +60,20 @@ void CSmoothEmulator::Tune(){
 			for(ic=NTrainingPts;ic<NCoefficients;ic++){
 				betaadotbetab+=beta(a,ic)*beta(b,ic);
 			}
-			delta(b)+=betadotbetab*alpha(a);
+			delta(b)+=betaadotbetab*alpha(a);
 		}
 	}
 	gamma=C.colPivHouseholderQr().solve(delta);
-	for(ic=NTrainingPts;ic<Coefficients;ic++){
-		Abest(ic)=0.0;
+	for(ic=NTrainingPts;ic<NCoefficients;ic++){
+		Abest[ic]=0.0;
 		for(a=0;a<NTrainingPts;a++){
-			Abest(ic)+=gamma(a)*beta(a,ic);
+			Abest[ic]+=gamma(a)*beta(a,ic);
 		}
 	}
 	for(a=0;a<NTrainingPts;a++){
-		Abest(a)=alpha(a);
-		for(ic=NTrainingPts;ic<NCoefficents;ic++){
-			Abest(a)-=beta(a,ic)*Abest(ic);
+		Abest[a]=alpha(a);
+		for(ic=NTrainingPts;ic<NCoefficients;ic++){
+			Abest[a]-=beta(a,ic)*Abest[ic];
 		}
 	}
 }
